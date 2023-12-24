ae0131eaf3bb386ee6303a4d62853fb9bf6b393a
Egemen Çapacıoğlu <42514430+egemencpcgl@users.noreply.github.com>
2023-12-24 03:32:36 +0300

    Rapor servisi eklendi.
    Rapor servis temel seviyede hazır şekilde ve bu eklemede mqtt client implamentasyonu hazır olarak gönderiliyor.

---
+++ ReportManagementService/Dtos/ReportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ReportManagementService.Dtos
+{
+    public class ReportDto
+    {
+        public Guid Id { get; set; }
+        public string ReportName { get; set; }
+        public string ReportStatus { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
---
+++ ReportManagementService/Interfaces/IReportService.cs
@@ -0,0 +1,15 @@
+﻿using ReportManagementService.Dtos;
+using ReportManagementService.Models;
+using ReportManagementService.Responses;
+
+namespace ReportManagementService.Interfaces
+{
+    public interface IReportService
+    {
+        Report CreatReport(ReportDto reportDto);
+        Task<Response<List<ReportDto>>> GetStatisticsAllLocation();
+        Task<Response<ReportDto>> GetStatisticsByLocation(string location);
+        Task<Response<List<ReportDto>>> GetAllReadyReports();
+        Task<Response<List<ReportDetailDto>>> GetReadyReportDetail(Guid reportID);
+    }
+}
