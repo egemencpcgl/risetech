9661cbc82faed67f356cd2cb1074e300cd0cf3e3
Egemen Çapacıoğlu <42514430+egemencpcgl@users.noreply.github.com>
2024-01-15 18:57:41 +0300

    dosya ve docker image resimleri düzenlendi

---
+++ src/ReportService/Models/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace ReportManagementService.Models
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+        public string ReportName { get; set; }
+        public string ReportStatus { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+        public Report()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/ReportService/Models/ReportDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace ReportManagementService.Models
+{
+    public class ReportDetail
+    {
+        public Guid Id { get; set; }
+        public Guid ReportId { get; set; }
+        public string City { get; set; }
+        public int PeopleCount { get; set; }
+        public int PhoneCount { get; set; }
+
+        public ReportDetail()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
